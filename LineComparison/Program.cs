1914d8d4973257de212db95a48da718cac4175b8
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-06 13:21:47 +0530

    [Ritvik]: Added Length of a Line

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            Console.WriteLine("Welcome to Line Comparison Computation Program\n");
+
+            int x1 = 2;
+            int x2 = 4;
+            int y1 = 6;
+            int y2 = 8;
+
+            double lengthOfALine = 0;
+
+            lengthOfALine = Math.Sqrt(Math.Pow(x2 - x1,2) + Math.Pow(y2 - y1,2));
+
+            Console.WriteLine("Length of a Line is : " + lengthOfALine);
         }
     }
 }
